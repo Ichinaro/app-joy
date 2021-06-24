673df73a7558d1b3df38d58cb7bb0fa65afe92d9
Ichinaro <65051884+Ichinaro@users.noreply.github.com>
2021-06-23 19:22:09 -0500

    Update ArtItem.jsx

--- src/frontend/components/ArtItem.jsx
+++ src/frontend/components/ArtItem.jsx
@@ -5,7 +5,7 @@ const ArtItem = (props) => {
   return (
     <>
       <Link to={`/articulo/${props.articulo._id}`}>
-        <img src={props.articulo.url} alt="" />
+        <img src={props.articulo.img} alt="" />
       </Link>
       <div className="text-articulo">
         <h2>{props.articulo.titulo}</h2>
